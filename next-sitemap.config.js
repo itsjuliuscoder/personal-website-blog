4987bd679c0639515681d7d503330772215b4d76
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2024-10-25 03:15:44 +0100

    tiny fix on sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 // next-sitemap.config.js
 module.exports = {
-  siteUrl: 'https://www.juliusolajumoke.com', // Replace with your domain
+  siteUrl: 'https://juliusolajumoke.com', // Replace with your domain
   generateRobotsTxt: true, // (optional)
 };
